17c64b0eb141a754f9e95e6a083525cc06ca2b7c
Rene Del Carpio <delcarpio.rene@gmail.com>
2021-08-16 22:40:54 -0400

    context was add. styles was add

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,63 +1,217 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { ThemeContext } from '../context/ThemeContext';
 import '../scss/buttons.scss';
 
 const Buttons = () => {
+	let { theme } = useContext(ThemeContext);
 	return (
-		<div className='buttons-container'>
-			<div className='button'>
+		<div
+			className={
+				theme === 1
+					? 'buttons-container dark'
+					: theme === 2
+					? 'buttons-container light'
+					: 'buttons-container third'
+			}
+		>
+			<button
+				className={
+					theme === 1
+						? 'button dark'
+						: theme === 2
+						? 'button light'
+						: 'button third'
+				}
+			>
 				<h1>7</h1>
-			</div>
-			<div className='button'>
+			</button>
+			<button
+				className={
+					theme === 1
+						? 'button dark'
+						: theme === 2
+						? 'button light'
+						: 'button third'
+				}
+			>
 				<h1>8</h1>
-			</div>
-			<div className='button'>
+			</button>
+			<button
+				className={
+					theme === 1
+						? 'button dark'
+						: theme === 2
+						? 'button light'
+						: 'button third'
+				}
+			>
 				<h1>9</h1>
-			</div>
-			<div className='button__del'>
+			</button>
+			<button
+				className={
+					theme === 1
+						? 'button__del dark'
+						: theme === 2
+						? 'button__del light'
+						: 'button__del third'
+				}
+			>
 				<h1>DEL</h1>
-			</div>
-			<div className='button'>
+			</button>
+			<button
+				className={
+					theme === 1
+						? 'button dark'
+						: theme === 2
+						? 'button light'
+						: 'button third'
+				}
+			>
 				<h1>4</h1>
-			</div>
-			<div className='button'>
+			</button>
+			<button
+				className={
+					theme === 1
+						? 'button dark'
+						: theme === 2
+						? 'button light'
+						: 'button third'
+				}
+			>
 				<h1>5</h1>
-			</div>
-			<div className='button'>
+			</button>
+			<button
+				className={
+					theme === 1
+						? 'button dark'
+						: theme === 2
+						? 'button light'
+						: 'button third'
+				}
+			>
 				<h1>6</h1>
-			</div>
-			<div className='button'>
+			</button>
+			<button
+				className={
+					theme === 1
+						? 'button dark'
+						: theme === 2
+						? 'button light'
+						: 'button third'
+				}
+			>
 				<h1>+</h1>
-			</div>
-			<div className='button'>
+			</button>
+			<button
+				className={
+					theme === 1
+						? 'button dark'
+						: theme === 2
+						? 'button light'
+						: 'button third'
+				}
+			>
 				<h1>1</h1>
-			</div>
-			<div className='button'>
+			</button>
+			<button
+				className={
+					theme === 1
+						? 'button dark'
+						: theme === 2
+						? 'button light'
+						: 'button third'
+				}
+			>
 				<h1>2</h1>
-			</div>
-			<div className='button'>
+			</button>
+			<button
+				className={
+					theme === 1
+						? 'button dark'
+						: theme === 2
+						? 'button light'
+						: 'button third'
+				}
+			>
 				<h1>3</h1>
-			</div>
-			<div className='button'>
+			</button>
+			<button
+				className={
+					theme === 1
+						? 'button dark'
+						: theme === 2
+						? 'button light'
+						: 'button third'
+				}
+			>
 				<h1>-</h1>
-			</div>
-			<div className='button'>
+			</button>
+			<button
+				className={
+					theme === 1
+						? 'button dark'
+						: theme === 2
+						? 'button light'
+						: 'button third'
+				}
+			>
 				<h1>.</h1>
-			</div>
-			<div className='button'>
+			</button>
+			<button
+				className={
+					theme === 1
+						? 'button dark'
+						: theme === 2
+						? 'button light'
+						: 'button third'
+				}
+			>
 				<h1>0</h1>
-			</div>
-			<div className='button'>
+			</button>
+			<button
+				className={
+					theme === 1
+						? 'button dark'
+						: theme === 2
+						? 'button light'
+						: 'button third'
+				}
+			>
 				<h1>/</h1>
-			</div>
-			<div className='button'>
+			</button>
+			<button
+				className={
+					theme === 1
+						? 'button dark'
+						: theme === 2
+						? 'button light'
+						: 'button third'
+				}
+			>
 				<h1>x</h1>
-			</div>
-			<div className='button__reset'>
+			</button>
+			<button
+				className={
+					theme === 1
+						? 'button__reset dark'
+						: theme === 2
+						? 'button__reset light'
+						: 'button__reset third'
+				}
+			>
 				<h1>RESET</h1>
-			</div>
-			<div className='button__equal'>
+			</button>
+			<button
+				className={
+					theme === 1
+						? 'button__equal dark'
+						: theme === 2
+						? 'button__equal light'
+						: 'button__equal third'
+				}
+			>
 				<h1>=</h1>
-			</div>
+			</button>
 		</div>
 	);
 };
--- src/components/Screen.js
+++ src/components/Screen.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import '../scss/screen.scss';
+import { ThemeContext } from '../context/ThemeContext';
 
 const Screen = () => {
+	let { theme } = useContext(ThemeContext);
 	return (
-		<div className='screen-container'>
+		<div
+			className={
+				theme === 1
+					? 'screen-container dark'
+					: theme === 2
+					? 'screen-container light'
+					: 'screen-container third'
+			}
+		>
 			<h1>Result</h1>
 		</div>
 	);
